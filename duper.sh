ede99b167f66997490092afea1214c2e00f368b4
Sascha Mendel <saschamendel2016@gmail.com>
2019-04-03 16:35:08 +0200

    Het script loopt nu door alle files in de opgegeven dir en stopt de shasum ervan in een array

--- duper.sh
+++ duper.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-
 # Maak een var directory als hij nog niet bestaat.
 
 mkdir -p ~/var
+
+
+# files houdt alle unieke shasums van files in om te kijken of we een bestand al hebben gehad
+files=()
+
+# duplicates houdt alle bestanden in 
+duplicates=()
+
 while getopts "nx:l:" opt; do
     case $opt in
     n)
@@ -15,6 +22,20 @@ while getopts "nx:l:" opt; do
     l)
         echo "l"
     ;;
-
     esac
 done
+
+# Zorgt dat we de $1 kunnen aanroepen door alle andere dingen weg te shiften.
+shift $((OPTIND-1))
+
+checkFile(){
+    shasum "$1"
+}
+
+export -f checkFile 
+files+=($(find "$1" -type f -exec bash -c 'checkFile "$0" &' {} \; | awk '{print $1}')) 
+
+# Wacht tot alle files
+wait
+printf '%s\n' "${files[@]}"
+
